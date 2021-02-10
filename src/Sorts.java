226432c41c3abde61633d12c9e6aed7c3062beef
Diane Li <dil014@ucsd.edu>
2021-02-09 22:29:13 -0800

    all files except part 1 pdf

---
+++ src/Sorts.java
@@ -0,0 +1,202 @@
+/*
+ * NAME: Diane Li
+ * PID:  A15773774
+ */
+import java.util.ArrayList;
+
+/**
+ * Sorts class.
+ * @param <T> Generic type
+ * @author Diane Li
+ * @since  02/09/2021
+ */
+public class Sorts<T extends Comparable<? super T>> {
+
+    private static final int MIDDLE_IDX = 2;
+    private static final int INCREASE_PARAM = 2;
+
+    /**
+     * This method performs insertion sort on the input arraylist
+     *
+     * @param list The arraylist we want to sort
+     * @param start The initial index on subsection of Arraylist we want to sort
+     * @param end The final index of the subsection of Arraylist we want to sort
+     */
+    public void InsertionSort(ArrayList<T> list, int start, int end) {
+        int j;
+        T temp, curEl, prevEl;
+        for (int i = 1; i <= end; i++) {
+            j = i;
+            while (j > 0 && list.get(j).compareTo(list.get(j - 1)) < 0) {
+                temp = list.get(j);
+                list.set(j, list.get(j - 1));
+                list.set(j - 1, temp);
+                j--;
+            }
+        }
+    }
+
+    /**
+     * This method performs merge sort on the input arraylist
+     *
+     * @param list The arraylist we want to sort
+     * @param start The initial index on subsection of Arraylist we want to sort
+     * @param end The final index of the subsection of Arraylist we want to sort
+     */
+    public void MergeSort(ArrayList<T> list, int start, int end) {
+        if (start < end)
+        {
+            int mid = start + (end - start) / MIDDLE_IDX;
+            MergeSort(list, start, mid);
+            MergeSort(list , mid + 1, end);
+
+            merge(list, start, mid, end);
+        }
+    }
+
+    /**
+     * merge helper function for MergeSort
+     *
+     * @param arr The arraylist we want to sort
+     * @param l left-most index we want to merge
+     * @param m the middle index we want to merge
+     * @param r right-most index we want to merge
+     */
+    private void merge(ArrayList<T> arr, int l, int m, int r) {
+
+        int mergedSize = r - l + 1;
+
+        ArrayList<T> mergedNums = new ArrayList<>();
+        int left = l, right = m + 1;
+        while (left <= m && right <= r) {
+            if (arr.get(left).compareTo(arr.get(right)) <= 0) {
+                mergedNums.add(arr.get(left));
+                left++;
+            }
+            else {
+                mergedNums.add(arr.get(right));
+                right++;
+            }
+        }
+
+        while (left <= m) {
+            mergedNums.add(arr.get(left));
+            left++;
+        }
+        while (right <= r) {
+            mergedNums.add(arr.get(right));
+            right++;
+        }
+        for (int i = 0; i < mergedSize; i++) {
+            arr.set(l + i, mergedNums.get(i));
+        }
+    }
+
+    /**
+     * This method performs quick sort on the input arraylist
+     *
+     * @param list The arraylist we want to sort
+     * @param start The initial index on subsection of Arraylist we want to sort
+     * @param end The final index of the subsection of Arraylist we want to sort
+     */
+    public void QuickSort(ArrayList<T> list, int start, int end) {
+        if (start < end) {
+            int lowEndIndex = partition(list, start, end);
+            QuickSort(list, start, lowEndIndex);
+            QuickSort(list, lowEndIndex + 1, end);
+        }
+    }
+
+    /**
+     * partition helper function for QuickSort
+     *
+     * @param arr The arraylist we want to sort
+     * @param l left-most index we want to merge
+     * @param h right-most index we want to merge
+     */
+    private int partition(ArrayList<T> arr, int l, int h) {
+        int mid = l + (h - l) / MIDDLE_IDX;
+        T pivot = arr.get(mid);
+        boolean done = false;
+        T temp, lowEl, highEl;
+
+        while (!done) {
+            // increment lowI while arr[lowI] < pivot
+            while (arr.get(l).compareTo(pivot) < 0) { l++; }
+
+            // decrement highI while pivot < arr[highI]
+            while (pivot.compareTo(arr.get(h)) < 0) { h--; }
+
+            // if zero or one elements remain, all numbers are partitioned
+            if (l >= h) { done = true; }
+            else {
+                // swap arr[lowI] and arr[highI]
+                temp = arr.get(l);
+                arr.set(l, arr.get(h));
+                arr.set(h, temp);
+
+                l++;
+                h--;
+            }
+        }
+        return h;
+    }
+
+    /**
+     * This method performs a modified QuickSort that switches to insertion sort after given cutoff
+     *
+     * @param list The arraylist we want to sort
+     * @param start The initial index on subsection of Arraylist we want to sort
+     * @param end The final index of the subsection of Arraylist we want to sort
+     * @param cutoff the minimum length of an subsection of the arraylist
+     *               such that we switch to Insertion Sort
+     */
+    public void Modified_QuickSort(ArrayList<T> list, int start, int end, int cutoff) {
+        if (start >= end) { return; }
+        else if ((end - start) + 1 <= cutoff) { this.InsertionSort(list, start, end); }
+
+        int lowEndIndex = partition(list, start, end);
+        QuickSort(list, start, lowEndIndex);
+        QuickSort(list, lowEndIndex + 1, end);
+    }
+
+    /**
+     * This method performs a modified QuickSort that switches to insertion sort
+     * after a certain cutoff
+     *
+     * @param list The arraylist we want to sort
+     * @param start The initial index on subsection of Arraylist we want to sort
+     * @param end The final index of the subsection of Arraylist we want to sort
+     * @param param The length of the initial splits that are sorted prior to merging
+     */
+    public void TimSort(ArrayList<T> list, int start, int end, int param){
+        int arrLen = end - start + 1;
+
+        // defaults ot InsertionSort param >= length of array
+        if (arrLen <= param) { this.InsertionSort(list, start, end); }
+
+        // otherwise, divide sub-arrays of length param and InsertionSort each
+        for (int i = start; i <= end; i += param) {
+            if (i + (param - 1) > end) { this.InsertionSort(list, i, end); }
+            else { this.InsertionSort(list, i, i + (param - 1));}
+        }
+
+        // sweep through array, merge sub-arrays and double sub-array size each iteration
+        int subarrLen = param * INCREASE_PARAM;
+        int idx;
+        while (subarrLen <= arrLen) {
+            idx = start;
+            while (idx + subarrLen - 1 < arrLen) {
+                this.merge(list, idx, idx + subarrLen / MIDDLE_IDX - 1, idx + subarrLen - 1);
+                idx += subarrLen;
+            }
+            subarrLen *= INCREASE_PARAM;
+        }
+
+        int leftoverSub = arrLen % (param * INCREASE_PARAM);
+        if (leftoverSub != 0) {
+            this.merge(list, start, end - leftoverSub, end);
+        }
+    }
+
+}
---
+++ src/SortsTest.java
@@ -0,0 +1,89 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class SortsTest {
+
+    Sorts obj;
+    ArrayList<String> names;
+    ArrayList<String> sortedNames;
+    ArrayList<Character> chars;
+    ArrayList<Character> sortedChars;
+    ArrayList<Integer> nums;
+    ArrayList<Integer> sortedNums;
+
+    @Before
+    public void setUp() throws Exception {
+        obj = new Sorts();
+
+        names = new ArrayList<>();
+        String[] namesToAdd = {"Marina", "Mar", "Diane", "Li", "Langlois"};
+        for (String name : namesToAdd) { names.add(name); }
+        sortedNames = new ArrayList<>();
+        String[] sortedNamesToAdd = {"Diane", "Langlois", "Li", "Mar", "Marina"};
+        for (String name : sortedNamesToAdd) { sortedNames.add(name); }
+
+        chars = new ArrayList<>();
+        char[] charsToAdd = {'Z', 'Y', 'X', 'D', 'C', 'B', 'A'};
+        for (char ch : charsToAdd) { chars.add(ch); }
+        sortedChars = new ArrayList<>();
+        char[] sortedCharsToAdd = {'A', 'B', 'C', 'D', 'X', 'Y', 'Z'};
+        for (char ch : sortedCharsToAdd) { sortedChars.add(ch); }
+
+        nums = new ArrayList<>();
+        sortedNums = new ArrayList<>();
+        int[] numsToAdd = {2, 4, 7, 10, 11, 32, 45, 78};
+        for (int num : numsToAdd) {
+            nums.add(num);
+            sortedNums.add(num);
+        }
+    }
+
+    @Test
+    public void testInsertionSort() {
+        obj.InsertionSort(names, 0, 4);
+        assertEquals(sortedNames, names);
+
+        obj.InsertionSort(chars, 0, 6);
+        assertEquals(sortedChars, chars);
+
+        obj.InsertionSort(nums, 0, 7);
+        assertEquals(sortedNums, nums);
+    }
+
+    @Test
+    public void testQuickSort() {
+        obj.QuickSort(names, 0, 4);
+        assertEquals(sortedNames, names);
+
+        obj.QuickSort(chars, 0, 6);
+        assertEquals(sortedChars, chars);
+
+        obj.QuickSort(nums, 0, 7);
+        assertEquals(sortedNums, nums);
+    }
+
+    @Test
+    public void testModified_QuickSort() {
+        obj.Modified_QuickSort(names, 0, 4, 1);
+        assertEquals(sortedNames, names);
+
+        obj.Modified_QuickSort(chars, 0, 6, 2);
+        assertEquals(sortedChars, chars);
+
+        obj.Modified_QuickSort(nums, 0, 7, 3);
+        assertEquals(sortedNums, nums);
+    }
+
+    @Test
+    public void testTimSort() {
+        obj.TimSort(names, 0, 4, 1);
+        assertEquals(sortedNames, names);
+
+        obj.TimSort(chars, 0, 6, 3);
+        assertEquals(sortedChars, chars);
+
+        obj.TimSort(nums, 0, 7, 3);
+        assertEquals(sortedNums, nums);
+    }
+}
